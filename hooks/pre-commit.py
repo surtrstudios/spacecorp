cbc378388c44756c63b4c3ca9adbe6584f840f66
Sean Kell <sean.michael.kell@gmail.com>
2013-05-09 09:20:56 -0400

    Add a 'rebuild' task to clean and build.
    Pre-commit hook should use the rebuild task.
    
    Issue #SPCRP-11 work 5m

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -9,7 +9,7 @@ def main():
 	# Stash any changes not in the current commit
 	subprocess.call(['git', 'stash', '-u', '--keep-index'], stdout=subprocess.PIPE)
 
-	subprocess.call(['gradle', '-q', 'all'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	subprocess.call(['gradle', '-q', 'rebuild'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 	# Unstash all changes 
 	subprocess.call(['git', 'reset', '--hard'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
