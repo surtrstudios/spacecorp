ec3a8a3d7bf0859be5de74b8d3c1ff2600e4ce2d
Sean Kell <sean.michael.kell@gmail.com>
2013-05-13 08:58:58 -0400

    Update bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y scons 
+apt-get install -y build-essential ruby1.9.1 ruby1.9.1-dev 
+gem install rdiscount --no-rdoc --no-ri 
+gem install jekyll
